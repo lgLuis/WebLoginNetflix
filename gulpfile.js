48f4a1ff2ee93c25892320dfe311824bc42d06c8
lgLuis <luis_unlz@yahoo.com.ar>
2021-07-18 17:19:52 -0300

    sass variables

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,15 @@ function css() {
         .pipe(sass({ outputStyle: 'expanded' }))
         .pipe(gulp.dest('css'))
 }
+ function wathArchivos(){
+    //  gulp.watch('scss/app.scss', css);
+     gulp.watch('scss/*.scss', css);
+     gulp.watch('index.html');
+
+ }
+
+
 
 //tarea
-
 gulp.task('css', css);
+gulp.task('watch', gulp.parallel(wathArchivos));
